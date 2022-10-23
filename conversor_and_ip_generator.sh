429af957305b27576dbb5b7765a3f2f5b56c34ec
h3g0c1v <66705453+h3g0c1v@users.noreply.github.com>
2022-10-23 16:41:42 +0200

    Updating File

--- conversor_and_ip_generator.sh
+++ conversor_and_ip_generator.sh
@@ -24,10 +24,12 @@ function helpPanel(){
     echo -e "\n${yellow}[i]${end}${gray} Uso:${end}"
 
     echo -e "\t${purple}-h${end}${gray} Mostrar este panel de ayuda${end}"
-    echo -e "\t${purple}-n${end}${gray} Generar un número del 0-255 aleatorio (en caso de introducir un número imprimirán los números hasta el introducido)${end}"
+    echo -e "\t${purple}-d${end}${gray} Generar un número decimal del 0-255 aleatorio${end}"
     echo -e "\t${purple}-b${end}${gray} Generar un número binario de 8 bits aleatorio${end}"
     echo -e "\t${purple}-x${end}${gray} Generar un número hexadecimal aleatorio${end}"
     echo -e "\t${purple}-i${end}${gray} Generar una dirección IP con la mascara de red de manera aleatoria${end}"
+    echo -e "\t${purple}-n${end}${gray} Generar una rista de números aleatorios dependiendo del número que hayas introducido${end}"
+    echo -e "\t${purple}-a${end}${gray} Generar números decimales, binarios y hexadecimales aleatoriamente (se utiliza junto con el -n)${end}"
 }
 
 function generate_number(){
@@ -39,7 +41,8 @@ function generate_number(){
         echo -e "\n${yellow}[+]${end}${gray} Elige una opción${end}"
         echo -e "\n\t${yellow}[+]${end}${gray} Solución en binario      ${end}${yellow}[1]${end}"
         echo -e "\t${yellow}[+]${end}${gray} Solución en hexadecimal  ${end}${yellow}[2]${end}"
-        echo -e "\t${yellow}[+]${end}${gray} Quiero hacer otra cosa   ${end}${yellow}[3]${end}"
+        echo -e "\t${yellow}[+]${end}${gray} Generar otro número      ${end}${yellow}[3]${end}"
+        echo -e "\t${yellow}[+]${end}${gray} Quiero hacer otra cosa   ${end}${yellow}[4]${end}"
         echo -ne "\n${yellow}[+]${end}${gray} ¿Que opción eliges? ->            ${yellow}" && read option
         echo -e "${end}"
 
@@ -94,6 +97,11 @@ function generate_number(){
         fi
 
         if [ "$option" -eq 3 ]; then
+            clear
+            generate_number
+        fi
+
+        if [ "$option" -eq 4 ]; then
 
             echo -e "\n${green}[!] ¡Saliendo!${end}"
             echo -e "${green}[+] Espero que te haya servido de ayuda :)${end}"
@@ -114,7 +122,8 @@ function generate_binary(){
         echo -e "\n${yellow}[+]${end}${gray} Elige una opción${end}"
         echo -e "\n\t${yellow}[+]${end}${gray} Solución en decimal      ${end}${yellow}[1]${end}"
         echo -e "\t${yellow}[+]${end}${gray} Solución en hexadecimal  ${end}${yellow}[2]${end}"
-        echo -e "\t${yellow}[+]${end}${gray} Quiero hacer otra cosa   ${end}${yellow}[3]${end}"
+        echo -e "\t${yellow}[+]${end}${gray} Generar otro número      ${end}${yellow}[3]${end}"
+        echo -e "\t${yellow}[+]${end}${gray} Quiero hacer otra cosa   ${end}${yellow}[4]${end}"
         echo -ne "\n${yellow}[+]${end}${gray} ¿Que opción eliges? ->            ${yellow}" && read option
         echo -e "${end}"
 
@@ -168,6 +177,11 @@ function generate_binary(){
         fi
 
         if [ "$option" -eq 3 ]; then
+            clear
+            generate_binary
+        fi
+        
+        if [ "$option" -eq 4 ]; then
 
             echo -e "\n${green}[!] ¡Saliendo!${end}"
             echo -e "${green}[+] Espero que te haya servido de ayuda :)${end}"
@@ -188,7 +202,8 @@ function generate_hex(){
         echo -e "\n${yellow}[+]${end}${gray} Elige una opción${end}"
         echo -e "\n\t${yellow}[+]${end}${gray} Solución en decimal      ${end}${yellow}[1]${end}"
         echo -e "\t${yellow}[+]${end}${gray} Solución en binario      ${end}${yellow}[2]${end}"
-        echo -e "\t${yellow}[+]${end}${gray} Quiero hacer otra cosa   ${end}${yellow}[3]${end}"
+        echo -e "\t${yellow}[+]${end}${gray} Generar otro número      ${end}${yellow}[3]${end}"
+        echo -e "\t${yellow}[+]${end}${gray} Quiero hacer otra cosa   ${end}${yellow}[4]${end}"
         echo -ne "\n${yellow}[+]${end}${gray} ¿Que opción eliges? ->            ${yellow}" && read option
         echo -e "${end}"
 
@@ -244,6 +259,11 @@ function generate_hex(){
         fi
 
         if [ "$option" -eq 3 ]; then
+            clear
+            generate_hex
+        fi
+
+        if [ "$option" -eq 4 ]; then
 
             echo -e "\n${green}[!] ¡Saliendo!${end}"
             echo -e "${green}[+] Espero que te haya servido de ayuda :)${end}"
@@ -260,16 +280,147 @@ function generate_ip_netmask(){
     echo -e "\n${purple}[+]${end}${gray} Dirección IP: ${end}${yellow}$ip/$random_subnet${end}"
 }
 
+function number_numbers(){
+
+    number=$1
+
+    if [ -z $number ]; then
+        echo hola
+    fi
+
+    echo -e "\n${yellow}[+]${end}${gray} Imprimiendo ${end}${yellow}$number${end}${gray} números decimales:${end}\n"
+    for quantity in $(seq -w 1 $number); do
+        random_number=$((RANDOM % 256))
+        echo -e "\t${purple}[$quantity]${end}${gray} Número decimal:${end} ${yellow}$random_number${end}"
+        let counter+=1
+    done
+    echo
+}
+
+function number_binaries(){
+    
+    number=$1
+    
+    echo -e "\n${yellow}[+]${end}${gray} Imprimiendo ${end}${yellow}$number${end}${gray} números binarios:${end}\n"
+    for quantity in $(seq -w 1 $number); do
+        random_number=$((RANDOM % 256))
+        binary=$(echo "obase=2;$random_number" | bc)
+        echo -e "\t${purple}[$quantity]${end}${gray} Número binario:${end} ${yellow}$binary${end}"
+        let counter+=1
+    done
+    echo
+}
+
+function number_hex(){
+
+    number=$1
+
+    echo -e "\n${yellow}[+]${end}${gray} Imprimiendo ${end}${yellow}$number${end}${gray} números hexadecimales:${end}\n"
+    for quantity in $(seq -w 1 $number); do
+        random_number=$((RANDOM % 256))
+        hex=$(echo "obase=16;$random_number" | bc)
+        echo -e "\t${purple}[$quantity]${end}${gray} Número decimal:${end} ${yellow}$hex${end}"
+        let counter+=1
+    done
+    echo
+}
+
+function generate_aleatory(){
+    
+    all=$1
+
+    while [ "$all" != "$quantity" ]; do
+        for quantity in $(seq -w 1 $1); do
+
+            random_number=$((RANDOM % 256))
+            random_number_binary=$((RANDOM % 256))
+            random_number_hex=$((RANDOM % 256))
+
+            decimal=$(echo "obase=10;$random_number" | bc)
+            solution_decimal_to_binary=$(echo "obase=2;$decimal" | bc)
+            solution_decimal_to_hex=$(echo "obase=16;$decimal" | bc)
+
+            binary=$(echo "obase=2;ibase=10;$random_number_binary" | bc)
+            solution_binary_to_decimal=$(echo "ibase=2;$binary;obase=10" | bc)
+            solution_binary_to_hex=$(echo "obase=16;ibase=2;$binary" | bc)
+
+            hex=$(echo "obase=16;ibase=10;$random_number_hex" | bc)
+            solution_hex_to_decimal=$(echo "obase=10;ibase=16;$hex" | bc)
+            solution_hex_to_binary=$(echo "obase=2;ibase=16;$hex" | bc)
+            
+            declare -i counter=1
+
+            echo -e "\n${yellow}[$quantity/$all]${end}${gray} Imprimiendo números aleatorios de cada:${end}\n"
+
+            echo -e "\t${purple}[$counter]${end}${gray} Número decimal:${end} ${yellow}$decimal${end}"
+            let counter+=1
+
+            echo -e "\t${purple}[$counter]${end}${gray} Número binario:${end} ${yellow}$binary${end}"
+            let counter+=1
+
+            echo -e "\t${purple}[$counter]${end}${gray} Número hexadecimal:${end} ${yellow}$hex${end}\n"
+            
+            echo -ne "${yellow}[+]${end}${gray} ¿Deseas mostrar las soluciones? ${end}${blue}[Y/N] (default: Y) ${end}${gray}-> ${end}" && read solutions
+
+            while [ $solutions != "N" ] && [ $solutions != "n" ] && [ $solutions != "Y" ] && [ $solutions != "y" ]; do
+
+                echo -e "\n${red}[!] Introduce una opción valida${end}"
+                echo -ne "${yellow}[+]${end}${gray} ¿Deseas mostrar las soluciones? ${end}${blue}[Y/N] (default Y) ${end}${gray}-> ${end}" && read solutions
+            done
+
+            if [ "$solutions" == "N" ] || [ "$solutions" == "n" ]; then
+
+                clear
+                continue
+            else
+
+                echo -e "\n${yellow}[+]${end}${gray} Imprimiendo las soluciones:\n"
+                
+                echo -e "\t${purple}[1]${end}${gray} Número decimal ${yellow}$decimal${end}${gray}:${end}"
+                echo -e "\t\t${purple}[+]${end}${gray} Solución en binario: ${end}${green}$solution_decimal_to_binary${end}"
+                echo -e "\t\t${purple}[+]${end}${gray} Solución en hexadecimal: ${end}${green}$solution_decimal_to_hex${end}"
+
+                echo -e "\n\t${purple}[2]${end}${gray} Número binario ${yellow}$binary${end}${gray}:${end}"
+                echo -e "\t\t${purple}[+]${end}${gray} Solución en decimal: ${end}${green}$solution_binary_to_decimal${end}"
+                echo -e "\t\t${purple}[+]${end}${gray} Solución en hexadecimal: ${end}${green}$solution_binary_to_hex${end}"
+
+                echo -e "\n\t${purple}[3]${end}${gray} Número hexadecimal ${yellow}$hex${end}${gray}:${end}"
+                echo -e "\t\t${purple}[+]${end}${gray} Solución en binario: ${end}${green}$solution_hex_to_binary${end}"
+                echo -e "\t\t${purple}[+]${end}${gray} Solución en decimal: ${end}${green}$solution_hex_to_decimal${end}"
+                
+                if [ "$all" != "$quantity" ]; then
+                    tput civis
+                    echo -ne "\n${green}[!] Presiona enter para continuar " && read continuing
+                    tput cnorm
+                fi
+
+                if [ != $continuing ] && [ "$all" != "$quantity" ]; then
+                    
+                    clear
+                    continue
+                fi
+            fi
+        done
+    done
+
+    echo -e "\n${green}[+] ¡Terminaste todas tus operaciones!${end}"
+    echo -e "${green}[+] Espero que te haya servido de ayuda :)${end}"
+    echo -e "\n${green}[+] Saliendo ...${end}"
+    exit 0
+}
+
 declare -i parameter_counter=0
 
-while getopts "hnbxir" arg; do
+while getopts "hdbxirn:a:" arg; do
     
     case $arg in
         h) ;;
-        n) $OPTARG; let parameter_counter+=1;;
+        d) $OPTARG; let parameter_counter+=1;;
         b) $OPTARG; let parameter_counter+=2;;
         x) $OPTARG; let parameter_counter+=3;;
         i) $OPTARG; let parameter_counter+=4;;
+        n) number="$OPTARG"; let parameter_counter+=5;;
+        a) all="$OPTARG"; let parameter_counter+=9
     esac
 done
 
@@ -285,6 +436,18 @@ elif [ $parameter_counter -eq 3 ]; then
 elif [ $parameter_counter -eq 4 ]; then
 
     generate_ip_netmask
+elif [ $parameter_counter -eq 6 ]; then
+
+    number_numbers "$number"
+elif [ $parameter_counter -eq 7 ]; then
+
+    number_binaries "$number"
+elif [ $parameter_counter -eq 8 ]; then
+
+    number_hex "$number"
+elif [ $parameter_counter -eq 9 ]; then
+
+    generate_aleatory "$all"
 else
 
     helpPanel
